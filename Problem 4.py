3a7463864b61bcb8db215d067ad54c0139adb90e
StrangeT <42526069+StrangeT@users.noreply.github.com>
2018-08-20 17:51:40 +1200

    Delete Problem 4.py

--- Problem 4.py
+++
@@ -1,9 +0,0 @@
-#Find the largest palindrome possible by multiplying two three digit numbers
-print(max(i*j for i in range(1,1000) for j in range(1,1000) if str(i*j) == str(i*j)[::-1]))
-
-
-#print
-#maximum of following arguments
-# i times j for i between 1 and 1000
-# for j between 1, 10000
-# if (palindrome) 
